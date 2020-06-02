c76e959cddca487f742b8a9c70917ad18c7610f7
Ilya Pleshakov <da-eto@ya.ru>
2020-06-02 16:29:03 +0300

    [wip] простой парсер данных о книге прямо в команде

--- src/Command/BookLoadCommand.php
+++ src/Command/BookLoadCommand.php
@@ -2,6 +2,7 @@
 
 namespace App\Command;
 
+use Prewk\XmlStringStreamer;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -28,9 +29,78 @@ class BookLoadCommand extends Command
         $io = new SymfonyStyle($input, $output);
         $path = $input->getArgument('path');
 
-        // TODO: заменить заглушку на работу с файлом
         if (!file_exists($path)) {
             $io->error(sprintf('Файл "%s" не найден.', $path));
+
+            return 1;
+        }
+
+        $streamer = XmlStringStreamer::createUniqueNodeParser($path, ['uniqueNode' => 'description']);
+        $nodeData = $streamer->getNode();
+
+        if ($nodeData === false) {
+            $io->error(sprintf('Файл "%s" не содержит элемент description.', $path));
+
+            return 1;
+        }
+
+        $description = new \SimpleXMLElement($nodeData, LIBXML_NOERROR);
+
+        $bookTitleNodes = $description->xpath('title-info/book-title');
+
+        if (count($bookTitleNodes) !== 1) {
+            $io->error('Не указано название');
+
+            return -1;
+        }
+
+        $bookTitle = (string) $bookTitleNodes[0];
+        $io->writeln('Название: ' . $bookTitle);
+
+
+        $langNodes = $description->xpath('title-info/lang');
+
+        if (count($langNodes) !== 1) {
+            $io->error('Не указан язык');
+
+            return -1;
+        }
+
+        $lang = (string) $langNodes[0];
+        $io->writeln('Язык: ' . $lang);
+
+        $authors = $description->xpath('title-info/author');
+
+        foreach ($authors as $author) {
+            $firstNameNodes = $author->xpath('first-name');
+            $lastNameNodes = $author->xpath('last-name');
+            $middleNameNodes = $author->xpath('middle-name');
+            $nickNameNodes = $author->xpath('nickname');
+
+            if ((count($firstNameNodes) !== 1 || count($lastNameNodes) !== 1) && count($nickNameNodes) === 0) {
+                $io->error('Не могу прочитать информацию об авторе');
+
+                return -1;
+            }
+
+            if (count($firstNameNodes) === 1 && count($lastNameNodes) === 1) {
+                if (count($middleNameNodes) > 0) {
+                    $authorName = implode(' ', [
+                        (string) $firstNameNodes[0],
+                        (string) $middleNameNodes[0],
+                        (string) $lastNameNodes[0],
+                    ]);
+                } else {
+                    $authorName = implode(' ', [
+                        (string) $firstNameNodes[0],
+                        (string) $lastNameNodes[0],
+                    ]);
+                }
+            } else {
+                $authorName = (string) $nickNameNodes[0];
+            }
+
+            $io->writeln('Автор: ' . $authorName);
         }
 
         $io->success('Книга успешно загружена.');
